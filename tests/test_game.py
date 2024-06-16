b97942ba5f49ed3a94d882d4bdfe0d8bf6ef0dd5
Abhishek Verma <abhi.verma9420@gmail.com>
2024-06-16 17:03:18 +0000

    Feature complete: Dictionary check of attempt

--- tests/test_game.py
+++ tests/test_game.py
@@ -48,5 +48,5 @@ class TestGame:
     def test_unknown_word_is_invalid(self):
         """A word that is not in the English dictionary should not be valid"""
         new_game = Game()
-        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
-        assert new_game.is_valid('FEUN') is False
+        new_game.grid = list("KWIENFUQW")  # Force the grid to a test case:
+        assert new_game.is_valid("FEUN") is False
